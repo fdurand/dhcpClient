e2bbe8c18ee998478af8a112eedaf4e9a704b96c
Durand Fabrice <oeufdure@gmail.com>
2025-01-30 14:32:02 -0500

    First draft

---
+++ go.mod
@@ -0,0 +1,21 @@
+module dhcpclient
+
+go 1.22.4
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/krolaw/dhcp4 v0.0.0-20190909130307-a50d88189771
+	github.com/mdlayher/ethernet v0.0.0-20220221185849-529eae5b6118
+	github.com/mdlayher/raw v0.1.0
+	gopkg.in/ini.v1 v1.67.0
+)
+
+require (
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/mdlayher/packet v1.0.0 // indirect
+	github.com/mdlayher/socket v0.2.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+)
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/krolaw/dhcp4"
+	"gopkg.in/ini.v1"
+)
+
+type Interface struct {
+	Name     string
+	intNet   *net.Interface
+	ServerIP net.IP
+}
+
+// Options Struct
+type Options struct {
+	Option dhcp4.OptionCode `json:"option"`
+	Value  string           `json:"value"`
+	Type   string           `json:"type"`
+}
+
+func (d *Interface) readConfig() {
+
+	cfg, err := ini.Load("./godhcpclient.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	Interface := cfg.Section("interface").Key("listen").String()
+	d.intNet, err = net.InterfaceByName(Interface)
+	if err != nil {
+		fmt.Printf("Fail to find network interface:%v, %v", Interface, err)
+		os.Exit(1)
+	}
+
+	Server := cfg.Section("interface").Key("server").String()
+	d.ServerIP = net.ParseIP(Server)
+}
+
+func main() {
+	var d Interface
+	d.readConfig()
+
+	// Random xid
+	xid := make([]byte, 4)
+	rand.Read(xid)
+
+	// Add options
+	var options = Options{}
+
+	// Read options from json file
+	dhcpOptions := options.ReadOptions()
+	spew.Dump(dhcpOptions)
+	// Request IP address
+
+	packet := dhcp4.RequestPacket(dhcp4.Request, d.intNet.HardwareAddr, net.IPv4(0, 0, 0, 0), xid, true, dhcpOptions)
+	spew.Dump(packet)
+	Client, err := NewRawClient(d.intNet)
+	if err != nil {
+		fmt.Printf("Error : %s", err)
+		panic(err)
+	}
+	broadcastMAC, err := net.ParseMAC("FF:FF:FF:FF:FF:FF")
+	if err != nil {
+		fmt.Printf("Error : %s", err)
+		panic(err)
+	}
+
+	err = Client.sendDHCP(broadcastMAC, packet, d.ServerIP, net.IPv4zero)
+}
+
+func (a *Options) ReadOptions() []dhcp4.Option {
+
+	DHCPOptions := []Options{}
+	var dhcpOptions = []dhcp4.Option{}
+
+	body, err := os.ReadFile("Options.json")
+	if err != nil {
+		fmt.Printf("Error : %s", err)
+		panic(err)
+	}
+	err = json.Unmarshal(body, &DHCPOptions)
+	if err != nil {
+		fmt.Printf("Error : %s", err)
+		panic(err)
+	}
+	for _, option := range DHCPOptions {
+		var dhcpOption = dhcp4.Option{}
+		var Value interface{}
+		switch option.Type {
+		case "ipaddr":
+			Value = net.ParseIP(option.Value)
+			dhcpOption.Code = option.Option
+			dhcpOption.Value = Value.(net.IP).To4()
+		case "string":
+			Value = option.Value
+			dhcpOption.Code = option.Option
+			dhcpOption.Value = []byte(Value.(string))
+		case "int":
+			Value = option.Value
+			val, _ := strconv.Atoi(Value.(string))
+			bs := make([]byte, 4)
+			binary.BigEndian.PutUint32(bs, uint32(val))
+			dhcpOption.Code = option.Option
+			dhcpOption.Value = bs
+		case "bytes":
+			dhcpOption.Code = option.Option
+			for _, value := range strings.Split(option.Value, ",") {
+				spew.Dump(value)
+				val, _ := strconv.Atoi(value)
+				dhcpOption.Value = append(dhcpOption.Value, byte(val))
+			}
+			spew.Dump(dhcpOption.Value)
+		}
+		dhcpOptions = append(dhcpOptions, dhcpOption)
+	}
+	return dhcpOptions
+}
